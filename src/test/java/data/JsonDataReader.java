c59111272ba82b29822d9d45e916904e107a80d5
a7mdsamir <a99samir99@gmail.com>
2025-01-11 07:31:14 +0200

    CommitJsonDataReader

--- src/test/java/data/JsonDataReader.java
+++ src/test/java/data/JsonDataReader.java
@@ -7,7 +7,9 @@ import org.json.simple.parser.ParseException;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Random;
 
 
 public class JsonDataReader 
@@ -40,6 +42,33 @@ public class JsonDataReader
 
 		}
 
+	}
+
+	public void updateJsonFile() throws IOException, ParseException {
+		String filePath = System.getProperty("user.dir") + "/src/test/java/data/UserData.json";
+		File srcFile = new File(filePath);
+
+		JSONParser parser = new JSONParser();
+		JSONArray jarray = (JSONArray) parser.parse(new FileReader(srcFile));
+
+		// Generate random phone number and update it in the JSON object
+		Random random = new Random();
+		String random3Number = String.format("%03d", random.nextInt(100));
+		String newUsername = "ahmed" + random3Number;
+		for (Object jsonObj : jarray) {
+			JSONObject person = (JSONObject) jsonObj;
+
+
+			person.put("username", newUsername);
+
+			// Update the JSON file
+			try (FileWriter file = new FileWriter(filePath)) {
+				file.write(jarray.toJSONString());
+				file.flush();
+			}
+		}
+
+		System.out.println("JSON file updated with new random username." + newUsername);
 	}
 
 }
